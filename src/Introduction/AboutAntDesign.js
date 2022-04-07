7f9403b5e93ea8780eb6cdf815e05bb1abddd2ee
dyeeee <duye99@outlook.com>
2022-04-07 16:42:01 +0800

    Introduction page

--- src/Introduction/AboutAntDesign.js
+++ src/Introduction/AboutAntDesign.js
@@ -3,91 +3,100 @@ import { Typography, Divider } from 'antd';
 
 const { Title, Paragraph, Text, Link } = Typography;
 
-const blockContent = `AntV 是蚂蚁金服全新一代数据可视化解决方案，致力于提供一套简单方便、专业可靠、不限可能的数据可视化最佳实践。得益于丰富的业务场景和用户需求挑战，AntV 经历多年积累与不断打磨，已支撑整个阿里集团内外 20000+ 业务系统，通过了日均千万级 UV 产品的严苛考验。
-我们正在基础图表，图分析，图编辑，地理空间可视化，智能可视化等各个可视化的领域耕耘，欢迎同路人一起前行。`;
-
-
 export default function AboutAntDesign () {
   return (
     <Typography>
-      <Title>Introduction</Title>
-      <Paragraph>
-        In the process of internal desktop applications development, many different design specs and
-        implementations would be involved, which might cause designers and developers difficulties and
-        duplication and reduce the efficiency of development.
-      </Paragraph>
-      <Paragraph>
-        After massive project practice and summaries, Ant Design, a design language for background
-        applications, is refined by Ant UED Team, which aims to <Text strong>
-          uniform the user interface specs for internal background projects, lower the unnecessary
-          cost of design differences and implementation and liberate the resources of design and
-          front-end development</Text>.
-      </Paragraph>
-      <Title level={2}>Guidelines and Resources</Title>
-      <Paragraph>
-        We supply a series of design principles, practical patterns and high quality design resources
-        (<Text code>Sketch</Text> and <Text code>Axure</Text>), to help people create their product
-        prototypes beautifully and efficiently.
-      </Paragraph>
-
+      <Title level={3}>Ant Design <Link href="https://ant.design/">(Official site)</Link></Title>
       <Paragraph>
         <ul>
           <li>
-            <Link href="/docs/spec/proximity">Principles</Link>
+            A design system for enterprise-level products.
           </li>
           <li>
-            <Link href="/docs/spec/overview">Patterns</Link>
+            Stable and highly reusable components and pages.
           </li>
           <li>
-            <Link href="/docs/resources">Resource Download</Link>
+            Uniform the user interface specs and reduce redundancies
+          </li>
+          <li>
+            Support for multiple frameworks
           </li>
         </ul>
       </Paragraph>
-
+      <Divider />
+      <Title level={3}>Ant Design of React</Title>
       <Paragraph>
-        Press <Text keyboard>Esc</Text> to exit...
+        <img width="150" src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"></img><span>+</span><img width="160" src="https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg"></img>
+
+        <ul>
+          <li>
+            A React UI library that contains a set of high quality components
+            <ul>
+              <li>
+                Develop using TypeScript
+              </li>
+              <li>
+                Compatible with all modern browsers
+              </li>
+              <li>
+                Internationalized languages supported
+              </li>
+              <li>
+                Open source on GitHub
+              </li>
+              <li>
+                Customize UI components
+              </li>
+            </ul>
+          </li>
+          <li>
+            Official tutorials and documentation
+            <ul>
+              <li>
+                <Link href="https://ant.design/docs/react/introduce">https://ant.design/docs/react/introduce</Link>
+              </li>
+              <li>
+                See component effect and source code
+
+              </li>
+              <li>
+                Test in online editor
+              </li>
+
+            </ul>
+          </li>
+        </ul>
       </Paragraph>
+      <Divider />
+      <Title level={3}>About This Tech Demo <Link href="https://github.com/UOA-CS732-SE750-Students-2022/assignment-01-dyeeee">(Details in GitHub)</Link></Title>
+      <Paragraph>
+        In this project, I built a Demo website based on React and introduced the use of multiple Ant Design components in three parts: Basic components, Data Display components, and Feedback components. In addition, I used the relevant components to build a Pokemon illustrated page, similar to Execise5 in the classroom. Still, we can see that with the help of Ant Design, my UI interface has become very nice.
+      </Paragraph>
+
+      <ul>
+        <li>
+          A React App with Ant Design UI Library
+        </li>
+        <li>
+          Components of AntDesign
+          <ul>
+            <li>
+              Basic Components
+            </li>
+            <li>
+              Data Display Components
+            </li>
+            <li>
+              Feedback Components
+            </li>
+          </ul>
+        </li>
+        <li>
+          Pokedex UI Design
+        </li>
+      </ul>
 
       <Divider />
-
-      <Title>介绍</Title>
-      <Paragraph>
-        蚂蚁的企业级产品是一个庞大且复杂的体系。这类产品不仅量级巨大且功能复杂，而且变动和并发频繁，常常需要设计与开发能够快速的做出响应。同时这类产品中有存在很多类似的页面以及组件，可以通过抽象得到一些稳定且高复用性的内容。
-      </Paragraph>
-      <Paragraph>
-        随着商业化的趋势，越来越多的企业级产品对更好的用户体验有了进一步的要求。带着这样的一个终极目标，我们（蚂蚁金服体验技术部）经过大量的项目实践和总结，逐步打磨出一个服务于企业级产品的设计体系
-        Ant Design。基于<Text mark>『确定』和『自然』</Text>
-        的设计价值观，通过模块化的解决方案，降低冗余的生产成本，让设计者专注于
-        <Text strong>更好的用户体验</Text>。
-      </Paragraph>
-      <Title level={2}>设计资源</Title>
-      <Paragraph>
-        我们提供完善的设计原则、最佳实践和设计资源文件（<Text code>Sketch</Text> 和
-        <Text code>Axure</Text>），来帮助业务快速设计出高质量的产品原型。
-      </Paragraph>
-
-      <Paragraph>
-        <ul>
-          <li>
-            <Link href="/docs/spec/proximity-cn">设计原则</Link>
-          </li>
-          <li>
-            <Link href="/docs/spec/overview-cn">设计模式</Link>
-          </li>
-          <li>
-            <Link href="/docs/resources-cn">设计资源</Link>
-          </li>
-        </ul>
-      </Paragraph>
-
-      <Paragraph>
-        <blockquote>{blockContent}</blockquote>
-        <pre>{blockContent}</pre>
-      </Paragraph>
-
-      <Paragraph>
-        按<Text keyboard>Esc</Text>键退出阅读……
-      </Paragraph>
     </Typography>
   )
 }
--- src/Introduction/InstallAndInit.js
+++ src/Introduction/InstallAndInit.js
@@ -3,91 +3,39 @@ import { Typography, Divider } from 'antd';
 
 const { Title, Paragraph, Text, Link } = Typography;
 
-const blockContent = `AntV 是蚂蚁金服全新一代数据可视化解决方案，致力于提供一套简单方便、专业可靠、不限可能的数据可视化最佳实践。得益于丰富的业务场景和用户需求挑战，AntV 经历多年积累与不断打磨，已支撑整个阿里集团内外 20000+ 业务系统，通过了日均千万级 UV 产品的严苛考验。
-我们正在基础图表，图分析，图编辑，地理空间可视化，智能可视化等各个可视化的领域耕耘，欢迎同路人一起前行。`;
-
 
 export default function InstallAndInit () {
   return (
     <Typography>
-      <Title>ZZZZ</Title>
-      <Paragraph>
-        In the process of internal desktop applications development, many different design specs and
-        implementations would be involved, which might cause designers and developers difficulties and
-        duplication and reduce the efficiency of development.
-      </Paragraph>
-      <Paragraph>
-        After massive project practice and summaries, Ant Design, a design language for background
-        applications, is refined by Ant UED Team, which aims to <Text strong>
-          uniform the user interface specs for internal background projects, lower the unnecessary
-          cost of design differences and implementation and liberate the resources of design and
-          front-end development</Text>.
-      </Paragraph>
-      <Title level={2}>Guidelines and Resources</Title>
-      <Paragraph>
-        We supply a series of design principles, practical patterns and high quality design resources
-        (<Text code>Sketch</Text> and <Text code>Axure</Text>), to help people create their product
-        prototypes beautifully and efficiently.
-      </Paragraph>
+      <Title level={3}>Install Ant Design for React</Title>
+      <Paragraph>Execute the following installation command in the root directory of the react project</Paragraph>
+      <Text code>yarn add antd</Text>
 
-      <Paragraph>
-        <ul>
-          <li>
-            <Link href="/docs/spec/proximity">Principles</Link>
-          </li>
-          <li>
-            <Link href="/docs/spec/overview">Patterns</Link>
-          </li>
-          <li>
-            <Link href="/docs/resources">Resource Download</Link>
-          </li>
-        </ul>
-      </Paragraph>
+      <Title level={3}>Build this project</Title>
+      <Title level={5}>1. Node.js, React Framework and yarn  on your system </Title>
+      <Paragraph>As introduced in CS732</Paragraph>
 
-      <Paragraph>
-        Press <Text keyboard>Esc</Text> to exit...
-      </Paragraph>
+      <Title level={5}>2. Downlod project from Github</Title>
+      <ul>
+        <li>
+          https://github.com/UOA-CS732-SE750-Students-2022/assignment-01-dyeeee
+        </li>
+        <li>
+          Clone and Pull or Download Zip(suggested)
+        </li>
+      </ul>
 
-      <Divider />
+      <Title level={5}>3. Install React-Router for this project</Title>
+      <Text code>yarn add react-router-dom</Text>
 
-      <Title>介绍</Title>
-      <Paragraph>
-        蚂蚁的企业级产品是一个庞大且复杂的体系。这类产品不仅量级巨大且功能复杂，而且变动和并发频繁，常常需要设计与开发能够快速的做出响应。同时这类产品中有存在很多类似的页面以及组件，可以通过抽象得到一些稳定且高复用性的内容。
-      </Paragraph>
-      <Paragraph>
-        随着商业化的趋势，越来越多的企业级产品对更好的用户体验有了进一步的要求。带着这样的一个终极目标，我们（蚂蚁金服体验技术部）经过大量的项目实践和总结，逐步打磨出一个服务于企业级产品的设计体系
-        Ant Design。基于<Text mark>『确定』和『自然』</Text>
-        的设计价值观，通过模块化的解决方案，降低冗余的生产成本，让设计者专注于
-        <Text strong>更好的用户体验</Text>。
-      </Paragraph>
-      <Title level={2}>设计资源</Title>
-      <Paragraph>
-        我们提供完善的设计原则、最佳实践和设计资源文件（<Text code>Sketch</Text> 和
-        <Text code>Axure</Text>），来帮助业务快速设计出高质量的产品原型。
-      </Paragraph>
+      <Title level={5}>4. Install Ant Design for this project</Title>
+      <Text code>yarn add antd</Text>
 
-      <Paragraph>
-        <ul>
-          <li>
-            <Link href="/docs/spec/proximity-cn">设计原则</Link>
-          </li>
-          <li>
-            <Link href="/docs/spec/overview-cn">设计模式</Link>
-          </li>
-          <li>
-            <Link href="/docs/resources-cn">设计资源</Link>
-          </li>
-        </ul>
-      </Paragraph>
+      <Title level={5}>5. Initialize and Get start</Title>
+      <Text code>yarn</Text>
+      <br />
+      <Text code>yarn start</Text>
 
-      <Paragraph>
-        <blockquote>{blockContent}</blockquote>
-        <pre>{blockContent}</pre>
-      </Paragraph>
-
-      <Paragraph>
-        按<Text keyboard>Esc</Text>键退出阅读……
-      </Paragraph>
     </Typography>
   )
 }
